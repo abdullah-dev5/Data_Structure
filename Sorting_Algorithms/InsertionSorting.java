7f2b75a2be95eebd3c97235196232bfaa3211da7
abdullah-dev5 <imabdullah.tdm@gmail.com>
2023-09-14 16:40:38 +0500

    Create InsertionSorting.java

---
+++ Sorting_Algorithms/InsertionSorting.java
@@ -0,0 +1,40 @@
+
+//Used for Hybird Sorting Algorithms
+// Best Case O(n)
+// Worst Case O(N^2)
+
+import java.util.Arrays;
+
+public class InsertionSorting {
+
+    public static void main(String[] args) {
+
+        int[] arr = { 4, 7, 1, 3, 9, 23, 89, 12, 2 };
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertionSort(int arr[]) {
+        for (int i = 0; i < arr.length-1; i++) {
+
+            for (int j = i + 1; j > 0; j--) {
+
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+
+                } else {
+                    break;
+                }
+            }
+        }
+
+    }
+
+    private static void swap(int arr[], int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+
+}
