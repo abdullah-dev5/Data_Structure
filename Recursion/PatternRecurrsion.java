1575447bb93343cad4e1afd5ed1b0adedecf6b9f
abdullah-dev5 <imabdullah.tdm@gmail.com>
2023-09-25 00:10:05 +0500

    Create PatternRecurrsion.java

---
+++ Recursion/PatternRecurrsion.java
@@ -0,0 +1,26 @@
+public class PatternRecurrsion {
+
+    public static void main(String[] args) {
+
+        triangle(4, 0);
+
+    }
+
+    static void triangle(int row, int col) {
+        if (row == 0) {
+            return;
+        }
+        if (col < row) {
+            System.out.print(" * ");
+            triangle(row, col + 1);
+            // System.out.print(" * ");
+ 
+        } else {
+            System.out.println();
+            triangle(row - 1, 0);
+            // System.out.println();
+
+        }
+
+    }
+}
