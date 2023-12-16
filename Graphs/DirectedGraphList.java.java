94f197a58ba55370cc160b8030867720f64dc7c8
Muhammad Abdullah <113471263+abdullah-dev5@users.noreply.github.com>
2023-12-16 22:19:40 +0500

    Update DirectedGraphList.java.java

--- Graphs/DirectedGraphList.java.java
+++ Graphs/DirectedGraphList.java.java
@@ -3,14 +3,25 @@ import java.util.*;
 public class DirectedGraphList {
 
     private HashMap<Integer, ArrayList<Integer>> map;
+    private int numOfEdges;
+    private int numOfVertics;
 
     public DirectedGraphList() {
         map = new HashMap<Integer, ArrayList<Integer>>();
     }
 
+    public int getNumVertics() {
+        return numOfVertics;
+    }
+
+    public int getNumEdges() {
+        return numOfEdges;
+    }
+
     public void addVertex(int vertex) {
         if (!map.containsKey(vertex)) {
             map.put(vertex, new ArrayList<Integer>());
+            numOfVertics++;
         }
     }
 
@@ -22,6 +33,7 @@ public class DirectedGraphList {
             addVertex(vertex2);
         }
         map.get(vertex1).add(vertex2);
+        numOfEdges++;
     }
 
     public ArrayList<Integer> getAdjacentVertices(int vertex) {
@@ -34,24 +46,79 @@ public class DirectedGraphList {
     public void removeEdge(int vertex1, int vertex2) {
         if (map.containsKey(vertex1) && map.containsKey(vertex2)) {
             map.get(vertex1).remove(Integer.valueOf(vertex2));
+            numOfEdges--;
         }
     }
 
     public void removeVertex(int vertex) {
         if (map.containsKey(vertex)) {
-            // Remove the vertex from the map
-            map.remove(vertex); 
-            // Hashmap.keyset()  
+            map.remove(vertex);
+
+            // Hashmap.keyset()
             // Return Value: The method returns a set having the keys of the hash map.
             for (int key : map.keySet()) {
                 map.get(key).remove(Integer.valueOf(vertex));
             }
+            numOfVertics--;
         }
+    }
+     ArrayList<Integer> getNeighbor(int vertex)
+     {
+            ArrayList<Integer> neighbors =  new ArrayList<>(getAdjacentVertices(vertex));
+
+         for (int i = 0; i < map.size(); i++) {
+
+            if (map.get(i).size() > 0) {
+                for (int j = 0; j < map.get(i).size(); j++) {
+                    if (map.get(i).get(j) == vertex) {
+                    neighbors.add(i);
+                    }
+                }
+            }
+        }   
+
+            return neighbors;
+     }
+
+     void printNeighbor(int vertex)
+     {
+        System.out.print("List of Neighbors of "+vertex+" --> ");
+        for(int print : getNeighbor(vertex))
+        {
+            System.out.print(print+" ");
+        }
+        System.out.println();
+     }
+
+
+    void outgoingDegree(int vertex) {
+
+        ArrayList<Integer> outGoing = getAdjacentVertices(vertex);
+        System.out.print("Outgoing Degree of " + vertex + " --> ");
+        for (int key : outGoing) {
+            System.out.print(key + "  ");
+        }
+        System.out.println();
+    }
+
+    void inComingDegree(int vertex) {
+        int count = 0;
+        for (int i = 0; i < map.size(); i++) {
+
+            if (map.get(i).size() > 0) {
+                for (int j = 0; j < map.get(i).size(); j++) {
+                    if (map.get(i).get(j) == vertex) {
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println("Incoming Degree of " + vertex + " : " + count);
     }
 
     public void printGraph() {
         for (int vertex : map.keySet()) {
-            System.out.print(vertex + ": ");
+            System.out.print(vertex + " --> ");
             for (int adjacentVertex : map.get(vertex)) {
                 System.out.print(adjacentVertex + " ");
             }
@@ -69,7 +136,20 @@ public class DirectedGraphList {
 
         graph.printGraph();
         System.out.println();
-        graph.removeVertex(3);
+        graph.addVertex(6);
+        graph.addEdge(3, 6);
+        graph.addEdge(6, 5);
+
         graph.printGraph();
+        graph.getAdjacentVertices(5);
+
+        graph.outgoingDegree(5);
+        graph.inComingDegree(3);
+
+        graph.inComingDegree(5);
+        System.out.println(graph.getNumVertics());
+
+        graph.printNeighbor(5);
+       
     }
 }
